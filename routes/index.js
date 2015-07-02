845bc3b7cab34d6144707c72ad8629f95003820b
UnApice <unapice@yopmail.com>
2015-07-02 15:48:58 -0300

    cliente recibe JSON

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,10 +9,29 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/traer-eventos', function(req, res) {
-  	// devolver info al front 
   	console.log('llego pedido de cliente');
 
-  	res.end('wacho');
+  	// devolver info al front 
+  	// leer y enviar events.json
+  	// falta coordinar los formatos de envío y recepción para que 
+  	//   sea práctico insertar los eventos en los respectivos casillero-dia
+  	// supongo que 
+  	// próximo paso: filtrar los eventos para cierto user
+
+	fs.readFile('./events.json', function (err, data) {
+
+		if (err) throw err;
+
+		dataTest = {
+			"03-07-2015": "ir al super",
+			"04-07-2015": "futbol",
+			"10-07-2015": "lavar ropa"
+		};
+
+		res.contentType('application/json');
+		res.end(JSON.stringify(dataTest));
+	});
+  	// res.end('wacho');
 });
 
 module.exports = router;
