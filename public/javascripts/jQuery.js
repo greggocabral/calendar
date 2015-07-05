06ebf24acb31d5dac8e89e8471fd40d238fe584c
greggocabral <cabral.gr@gmail.com>
2015-07-05 18:59:03 -0300

    not important

--- public/javascripts/jQuery.js
+++ public/javascripts/jQuery.js
@@ -1,49 +1,49 @@
 $(document).ready(function() {
 
-	var dataTest = 'inicial';
+	var events = 'inicial';
 	// PEDIR CON UN GET A LA RUTA traer-eventos TODOS LOS EVENTOS DE EL USUARIO
 	$.get("/traer-eventos", function (data) {
-
-		console.log(data);
-
+		
 		dataTest = data;
 
 		// RENDERIZAR CALENDARIO VACIO CADA VEZ QUE SE CARGA LA PAGINA (ESTATICO)
-		for (var s=1; s<52; s++){
+		for (var s=1; s<52; s++){ //52
 			var id = "semana"+s;
 			$('.tabla-casilleros').append('<div id="'+id+'" class="row"></div>');
 
 			for (var i=1+(s-1)*7; i<8+(s-1)*7; i++){
-			 		var dia = moment().startOf('week').add(i,"days").format("DD");
-			 		var month = moment().startOf('week').add(i,"days").format("MM");
-			 		var year = moment().startOf('week').add(i,"days").format("YYYY");
-			 		var fullDate = dia + '-' + month + '-' + year;
-
+			 		var dia = moment().startOf('week').add(i,"days").format("D");
 			 		var mes = Number(moment().startOf('week').add(i,"days").format("M"));
+			 		var fullDate = moment().startOf('week').add(i,"days").format("DD-MM-YYYY");
+
+			 		
+
+			 		var isDayInThePast = moment("MM-DD-YYYY").diff(moment().startOf('week').add(i,"days"), 'days')>0; 
+			 		var isDayEven = (mes % 2 == 0);
 
 			 		//fecha identificacion del casillero
 			 		var dia_id = "" + fullDate;
-			 		//clase para dar formato al casillero dia, cambiando en funcion de paridad de mes 
-			 		var dia_class = (mes % 2 == 0 ? "dia-mes-par " : "") +' col-xs-2 casillero-dia'; 
+			 		//clase para dar formato al casillero dia, cambiando en funcion de paridad de mes y si es un dia del pasado
+			 		var dia_class = (isDayInThePast ? "dia-pasado " : (isDayEven? "dia-mes-par " : "")) +' col-xs-2 casillero-dia'; 
 			 		//texto a incorporar los dias 01 de cada mes. si no es 01, vale ""
 			 		var dia_texto_mes = (dia == 01 ? " " + moment().startOf('week').add(i,"days").format("MMMM").substring(0, 3) 
 		 												+ moment().startOf('week').add(i,"days").format("YY") : ""); 
 			 		//tasks del dia a incorporar. si no hay tasks vale ""
-			 		var dia_data = (typeof(dataTest[fullDate])!="undefined" ? "<h5 class='task'>" + dataTest[fullDate] + "</h5>" : "");
+			 		var dia_data = (typeof(events[fullDate])!="undefined" ? "<h5 class='task'>" + events[fullDate] + "</h5>" : "");
 		 			// append del casillero dia en cuestion
 		 			$("#semana"+s).append('<div id="'+ dia_id +'" class="'+ dia_class +'"> <h5>'+ dia +' ' + dia_texto_mes + dia_data + '</div>');
 		 		
 			}
 		}
 
-		// $(" div[id^='02-'] > h5:first-child").append("año");
+		
      
 		generadordeventos(); 
                     
      });
 
 	
-
+    
 
 	
 	function generadordeventos(){
@@ -107,6 +107,8 @@ $(document).ready(function() {
 	}	
 
 });
+
+
 
 
 
